f4a5945b5e1ce39c1c41cd21a3219e5e5545082c
Leon Kirschner <leon.kirschner@gmx.at>
2021-02-11 19:35:14 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+var b = chrome || browser
+
+b.contextMenus.create({
+    id: "search-instagram",
+    title: "Look it up on Instagram!",
+    contexts: ["selection"],
+});
+
+b.contextMenus.onClicked.addListener((info, _) => {
+    if (info.menuItemId == "search-instagram") {
+        b.tabs.create({
+            active: !0,
+            url: `https://www.instagram.com/${info.selectionText.trim()}/`
+        })
+    }
+})
